df4552129a7ca8cffb71a439851e8e77ebd7d410
satish4792 <satishkumar.iilm@gmail.com>
2017-11-14 14:58:35 +0530

    first commit

---
+++ src/com/sapariba/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.sapariba;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello how r u");
+		char c='*';
+		int n=5;
+		for (int i = 1; i <=10; i++) {
+			System.out.println(n*i);
+		}
+	}
+
+}
